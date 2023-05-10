154152eba4f9c1eee9b6c700cef6d98362566f19
lukas <lukas>
2023-05-10 12:48:23 +0200

    added subtract

--- basic_components.py
+++ basic_components.py
@@ -38,14 +38,68 @@ class Adder:
         self.input2 = input2
         self.full_adders = []
 
-
-        for bit1, bit2 in zip(self.input1, self.input2):
+        # Process the bits from right to left (LSB to MSB)
+        for bit1, bit2 in zip(reversed(self.input1), reversed(self.input2)):
             full_adder = FullAdder(bit1, bit2, carry_in)
             self.full_adders.append(full_adder)
             carry_in = full_adder.carry()
 
     def sum(self) -> list[bool]:
-        return [full_adder.sum() for full_adder in self.full_adders]
+        # Reverse the sum result to get the correct order of bits (MSB to LSB)
+        return list(reversed([full_adder.sum() for full_adder in self.full_adders]))
 
     def carry_out(self) -> bool:
         return self.full_adders[-1].carry()
+
+
+
+class HalfSubtractor:
+    #TODO add test
+    def __init__(self, input1:bool, input2:bool):
+        self.input1 = input1
+        self.input2 = input2
+    
+    def diff(self) -> bool:
+        return xor(self.input1, self.input2)
+ 
+    def borrow(self)->bool:
+        return and_(not_(self.input1), self.input2)
+    
+
+class FullSubtractor:
+    #TODO add test
+    def __init__(self, input1:bool, input2:bool, borrow_in:bool):
+        self.input1 = input1
+        self.input2 = input2
+        self.borrow_in = borrow_in
+        self.half_subtractor1 = HalfSubtractor(input1, input2)
+        self.half_subtractor2 = HalfSubtractor(self.half_subtractor1.diff(), borrow_in)
+
+    def diff(self):
+        return self.half_subtractor2.diff()
+    
+    def borrow(self):
+        return or_(self.half_subtractor1.borrow(), self.half_subtractor2.borrow())
+    
+
+class Subtractor:
+    def __init__(self, input1: list[bool], input2: list[bool], borrow_in: bool = False):
+        if len(input1) != 8 or len(input2) != 8:
+            raise ValueError("Both inputs must be 8 bits long.")
+        
+        self.input1 = input1
+        self.input2 = input2
+        self.full_subtractors = []
+
+        # Process the bits from right to left (LSB to MSB)
+        for bit1, bit2 in zip(reversed(self.input1), reversed(self.input2)):
+            full_subtractor = FullSubtractor(bit1, bit2, borrow_in)
+            self.full_subtractors.append(full_subtractor)
+            borrow_in = full_subtractor.borrow()
+
+    def diff(self) -> list[bool]:
+        # Reverse the sum result to get the correct order of bits (MSB to LSB)
+        return list(reversed([full_subtractor.diff() for full_subtractor in self.full_subtractors]))
+
+    def borrow_out(self) -> bool:
+        return self.full_subtractors[-1].borrow()
--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 
 import unittest
 import gates
-import basic_components
+from gates import and_, or_, not_, nand, nor, xor, xnor
+from basic_components import HalfAdder, FullAdder, Adder, HalfSubtractor, FullSubtractor, Subtractor
 import random
 
 class TestGates(unittest.TestCase):
@@ -62,7 +63,7 @@ class TestBasicComponents(unittest.TestCase):
         input2 = [random.choice([True, False]) for x in range(100)]
 
         for in1, in2 in zip(input1, input2):
-            half_adder = basic_components.HalfAdder(in1, in2)
+            half_adder = HalfAdder(in1, in2)
             if in1 and in2:
                 assert half_adder.carry() == True
                 assert half_adder.sum() == False
@@ -72,6 +73,7 @@ class TestBasicComponents(unittest.TestCase):
             else:
                 assert half_adder.carry() == False
                 assert half_adder.sum() == False
+ 
     def test_full_adder(self):
         """if all three inputs are true then carry is true and sum is true
         if two inputs are true then carry is true and sum is false
@@ -83,7 +85,7 @@ class TestBasicComponents(unittest.TestCase):
         carry_in = [random.choice([True, False]) for x in range(100)]
 
         for in1, in2, carry in zip(input1, input2, carry_in):
-            full_adder = basic_components.FullAdder(in1, in2, carry)
+            full_adder = FullAdder(in1, in2, carry)
             if in1 and in2 and carry:
                 assert full_adder.carry() == True
                 assert full_adder.sum() == True
@@ -96,18 +98,133 @@ class TestBasicComponents(unittest.TestCase):
             else:
                 assert full_adder.carry() == False
                 assert full_adder.sum() == False
+
     def test_adder(self):
-        """the adder takes in two 8 bit lists and an optional carry in and ads them"""
-        #randomly generate to 8 bit lists
-        input1 = [random.choice([True, False]) for x in range(8)]
-        input2 = [random.choice([True, False]) for x in range(8)]
+        """test the adder class multiple times with random values"""
+        
+        def test_adder_once():
+            """the adder takes in two 8-bit lists and an optional carry in and adds them"""
+            # randomly generate two 8-bit integers
+            int1 = random.randint(0, 255)
+            int2 = random.randint(0, 255)
 
-        #convert the lists to ints
-        int1 = int("".join([str(int(x)) for x in input1]), 2)
-        int2 = int("".join([str(int(x)) for x in input2]), 2)
+            # randomly generate a carry-in value
+            carry_in = random.choice([True, False])
 
-        adder = basic_components.Adder(input1, input2)
-        assert adder.sum() == 
+            # convert the integers to lists of boolean values
+            input1 = [bool(int(bit)) for bit in f"{int1:08b}"]
+            input2 = [bool(int(bit)) for bit in f"{int2:08b}"]
+
+            # use the Adder class to compute the sum with carry-in
+            adder = Adder(input1, input2, carry_in)
+            sum_result = adder.sum()
+            carry_out = adder.carry_out()
+
+            # compute the expected sum, account for carry-in
+            expected_sum = int1 + int2 + int(carry_in)
+
+            # expected carry-out value
+            expected_carry_out = expected_sum > 255
+
+            # apply modulo 256 for wrap around
+            expected_sum %= 256
+
+            # convert the sum result back to an integer
+            sum_result_int = int("".join([str(int(x)) for x in sum_result]), 2)
+
+            # compare the sum result with the expected sum
+            assert sum_result_int == expected_sum, f"Expected {expected_sum}, got {sum_result_int}"
+
+            # compare the carry-out value with the expected carry-out value
+            assert carry_out == expected_carry_out, f"Expected carry_out {expected_carry_out}, got {carry_out}"
+
+        for i in range(100):
+            test_adder_once()
+
+    def test_half_subtractor(self):
+        """Test various cases for HalfSubtractor"""
+
+        input1 = [random.choice([True, False]) for x in range(100)]
+        input2 = [random.choice([True, False]) for x in range(100)]
+
+        for in1, in2 in zip(input1, input2):
+            half_subtractor = HalfSubtractor(in1, in2)
+            expected_diff = xor(in1, in2)
+            expected_borrow = and_(not_(in1), in2)
+
+            self.assertEqual(half_subtractor.diff(), expected_diff)
+            self.assertEqual(half_subtractor.borrow(), expected_borrow)
+
+    def test_full_subtractor(self):
+        """Test various cases for FullSubtractor"""
+
+        input1 = [random.choice([True, False]) for x in range(100)]
+        input2 = [random.choice([True, False]) for x in range(100)]
+        borrow_in = [random.choice([True, False]) for x in range(100)]
+
+        for in1, in2, borrow in zip(input1, input2, borrow_in):
+            full_subtractor = FullSubtractor(in1, in2, borrow)
+
+            # Calculate expected diff and borrow
+            half_subtractor1 = HalfSubtractor(in1, in2)
+            half_subtractor2 = HalfSubtractor(half_subtractor1.diff(), borrow)
+
+            expected_diff = half_subtractor2.diff()
+            expected_borrow = or_(half_subtractor1.borrow(), half_subtractor2.borrow())
+
+            self.assertEqual(full_subtractor.diff(), expected_diff)
+            self.assertEqual(full_subtractor.borrow(), expected_borrow)
+
+
+    def test_half_subtractor(self):
+        """Test various cases for HalfSubtractor"""
+
+        input1 = [random.choice([True, False]) for x in range(100)]
+        input2 = [random.choice([True, False]) for x in range(100)]
+
+        for in1, in2 in zip(input1, input2):
+            half_subtractor = HalfSubtractor(in1, in2)
+            if in1 and not in2:
+                assert half_subtractor.borrow() == False
+                assert half_subtractor.diff() == True
+            elif not in1 and in2:
+                assert half_subtractor.borrow() == True
+                assert half_subtractor.diff() == True
+            else:
+                assert half_subtractor.borrow() == False
+                assert half_subtractor.diff() == False
+
+    def test_full_subtractor(self):
+        """Test various cases for FullSubtractor"""
+
+        input1 = [random.choice([True, False]) for x in range(100)]
+        input2 = [random.choice([True, False]) for x in range(100)]
+        borrow_in = [random.choice([True, False]) for x in range(100)]
+
+        for in1, in2, borrow in zip(input1, input2, borrow_in):
+            full_subtractor = FullSubtractor(in1, in2, borrow)
+            if in1 and not in2 and not borrow:
+                assert full_subtractor.borrow() == False, f"Inputs: {in1}, {in2}, {borrow}"
+                assert full_subtractor.diff() == True, f"Inputs: {in1}, {in2}, {borrow}"
+            elif not in1 and in2 and not borrow:
+                assert full_subtractor.borrow() == True, f"Inputs: {in1}, {in2}, {borrow}"
+                assert full_subtractor.diff() == True, f"Inputs: {in1}, {in2}, {borrow}"
+            elif not in1 and not in2 and borrow:
+                assert full_subtractor.borrow() == True, f"Inputs: {in1}, {in2}, {borrow}"
+                assert full_subtractor.diff() == True, f"Inputs: {in1}, {in2}, {borrow}"
+            elif in1 and in2 and borrow:
+                assert full_subtractor.borrow() == True, f"Inputs: {in1}, {in2}, {borrow}"
+                assert full_subtractor.diff() == True, f"Inputs: {in1}, {in2}, {borrow}"
+            elif (in1 and in2) or (in1 and borrow) or (in2 and borrow):
+                assert full_subtractor.borrow() == True, f"Inputs: {in1}, {in2}, {borrow}"
+                assert full_subtractor.diff() == False, f"Inputs: {in1}, {in2}, {borrow}"
+            elif in1 or in2 or borrow:
+                assert full_subtractor.borrow() == False, f"Inputs: {in1}, {in2}, {borrow}"
+                assert full_subtractor.diff() == True, f"Inputs: {in1}, {in2}, {borrow}"
+            else:
+                assert full_subtractor.borrow() == False, f"Inputs: {in1}, {in2}, {borrow}"
+                assert full_subtractor.diff() == False, f"Inputs: {in1}, {in2}, {borrow}"
+
 
 if __name__ == '__main__':
     unittest.main()
